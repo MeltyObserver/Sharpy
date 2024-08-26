fe3fd8d18eb445179b2cee3dd4c3a02eae94f85c
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2024-08-27 00:32:18 +0100

    added simple script for starting the site

---
+++ tools/watch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DOTNET_WATCH_SUPPRESS_EMOJIS=1
+export DOTNET_WATCH_RESTART_ON_RUDE_EDIT=1
+export DOTNET_WATCH_SUPPRESS_LAUNCH_BROWSER=1
+dotnet watch --project src/Sharpy.csproj # --non-interactive
