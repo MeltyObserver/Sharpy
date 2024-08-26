51f45df500fee438c8ad5d7b7c1cf17370cc4947
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2024-08-27 00:44:10 +0100

    added sidebar

--- src/AppSettings.cs
+++ src/AppSettings.cs
@@ -3,6 +3,7 @@ namespace Sharpy;
 public class AppSettings
 {
     public required string Title { get; set; }
+    public string Description { get; set; } = "Chirpy clone made in blazor";
     public string Image { get; set; } = "https://avatars.githubusercontent.com/u/164113402?v=4";
     public List<SocialMedia?>? SocialMedias { get; set; }
     public string DefaultTheme { get; set; } = "dark";
