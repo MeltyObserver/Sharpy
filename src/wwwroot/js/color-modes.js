f9d7149d07cccd356d991d2fc35ceb08bdedde3c
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2024-09-16 17:28:30 +0100

    minor changes

--- src/wwwroot/js/color-modes.js
+++ src/wwwroot/js/color-modes.js
@@ -4,7 +4,7 @@
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
  */
 
-// with minor edits :>
+// https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript
 
 (() => {
   'use strict'
@@ -14,18 +14,19 @@
 
   const getPreferredTheme = () => {
     const storedTheme = getStoredTheme()
+    console.log(storedTheme);
     if (storedTheme) {
       return storedTheme
     }
 
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'mocha' : 'latte'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
 
   const setTheme = theme => {
     if (theme === 'auto') {
-      document.documentElement.setAttribute('data-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
     } else {
-      document.documentElement.setAttribute('data-theme', theme)
+      document.documentElement.setAttribute('data-bs-theme', theme)
     }
   }
 
@@ -37,19 +38,18 @@
     if (!themeSwitcher) {
       return
     }
+
     const themeSwitcherText = document.querySelector('#bd-theme-text')
     const activeThemeIcon = document.querySelector('.theme-icon-active use')
-    const btnToActive = document.querySelector(`[data-theme-value="${theme}"]`)
+    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
     const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
 
-    document.querySelectorAll('[data-theme-value]').forEach(element => {
+    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
       element.classList.remove('active')
       element.setAttribute('aria-pressed', 'false')
     })
 
     btnToActive.classList.add('active')
-    // console.log(svgOfActiveBtn)
-
     btnToActive.setAttribute('aria-pressed', 'true')
     activeThemeIcon.setAttribute('href', svgOfActiveBtn)
     const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
@@ -70,11 +70,10 @@
   window.addEventListener('DOMContentLoaded', () => {
     showActiveTheme(getPreferredTheme())
 
-    document.querySelectorAll('[data-theme-value]')
+    document.querySelectorAll('[data-bs-theme-value]')
       .forEach(toggle => {
         toggle.addEventListener('click', () => {
-          const theme = toggle.getAttribute('data-theme-value')
-
+          const theme = toggle.getAttribute('data-bs-theme-value')
           setStoredTheme(theme)
           setTheme(theme)
           showActiveTheme(theme, true)
--- tools/watch.sh
+++ tools/watch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+export ASPNETCORE_ENVIRONMENT=Development
 export DOTNET_WATCH_SUPPRESS_EMOJIS=1
 export DOTNET_WATCH_RESTART_ON_RUDE_EDIT=1
 export DOTNET_WATCH_SUPPRESS_LAUNCH_BROWSER=1
+
 dotnet watch --project src/Sharpy.csproj # --non-interactive
