664d6ab1cafc004a25f9e92b89616a266f0e3d44
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2024-09-16 13:44:23 +0100

    added new class to hold site settings

---
+++ src/Models/SiteSettings.cs
@@ -0,0 +1,12 @@
+namespace Sharpy.Models;
+
+public static class SiteSettings
+{
+    public static class Pages
+    {
+        public const string Home = "Home";
+        public const string Projects = "Projects";
+        public const string About = "About";
+    }
+}
+
