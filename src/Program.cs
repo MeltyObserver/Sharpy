d1b37537ee563fa103ef7a35f40f26ad2cbc8042
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2024-09-16 17:24:02 +0100

    add posts and projects to be generated

--- src/Program.cs
+++ src/Program.cs
@@ -1,26 +1,49 @@
-using Sharpy;
-using BlazorStatic;
+using Sharpy.Models;
 using Sharpy.Components;
+using Sharpy.Extensions;
+
+using BlazorStatic;
+using Markdig;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions()
+        .UseYamlFrontMatter()
+        .UseCitations()
+        .UseBootstrap()
+        .UseBootstrapExtended()
+        // .UseHeadingsFormatter()
+        .Build();
+
 builder.Services.AddBlazorStaticService((o) =>
 {
+    o.MarkdownPipeline = pipeline;
+    o.OutputFolderPath = "../output";
     o.SuppressFileGeneration = true;
 });
 
 string appsettingsFile = builder.Environment.IsDevelopment() ? "appsettings.Development.json" : "appsettings.json";
 IConfiguration config = new ConfigurationBuilder().AddJsonFile(appsettingsFile).Build();
-AppSettings settings = config.GetRequiredSection("Settings").Get<AppSettings>()!;
+UserSettings settings = config.GetRequiredSection("Settings").Get<UserSettings>()!;
 
 builder.Services.AddSingleton(settings);
 
-var app = builder.Build();
+builder.Services.AddBlazorStaticContentService<PostFrontMatter>(opt =>
+{
+    opt.PageUrl = SiteSettings.Pages.Home;
+    opt.ContentPath = Path.Combine("Content", "Posts");
+});
 
-app.UseBlazorStaticGenerator();
+builder.Services.AddBlazorStaticContentService<ProjectFrontMatter>(opt =>
+{
+    opt.PageUrl = SiteSettings.Pages.Projects;
+    opt.ContentPath = Path.Combine("Content", SiteSettings.Pages.Projects);
+});
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -34,8 +57,7 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
+app.UseBlazorStaticGenerator(shutdownApp: !app.Environment.IsDevelopment());
 app.Run();
