3ba24a1fa3b47ef2a3aaf1790f50a91da85dfb5b
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2024-10-25 09:01:15 +0100

    moved the post list to a seperate page and added a link to it on the sidebar

--- src/Models/SiteSettings.cs
+++ src/Models/SiteSettings.cs
@@ -5,8 +5,8 @@ public static class SiteSettings
     public static class Pages
     {
         public const string Home = "Home";
+        public const string Posts = "Posts";
         public const string Projects = "Projects";
         public const string About = "About";
     }
 }
-
