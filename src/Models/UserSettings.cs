23097369444b257afca658aa77950bb558e21c3b
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2024-10-25 08:53:54 +0100

    added 'welcome text' property to user settings

--- src/Models/UserSettings.cs
+++ src/Models/UserSettings.cs
@@ -4,6 +4,7 @@ public class UserSettings
 {
     public required string Title { get; set; }
     public string Description { get; set; } = "Chirpy clone made in blazor";
+    public string WelcomeText { get; set; } = "Welcome to Sharpy";
     public string Image { get; set; } = "https://avatars.githubusercontent.com/u/164113402?v=4";
     public List<SocialMedia?>? SocialMedias { get; set; }
     public string DefaultTheme { get; set; } = "dark";
